2af6ec5c702fa752aa392e9f3b803594105be801
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Pro.local>
2020-07-25 20:32:49 -0400

    working in the imput for the map

--- gpsmark/src/components/Instructions.js
+++ gpsmark/src/components/Instructions.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
 
+const DirectionInstructions = (props) => {
+  if (props.directions.routes) {
+    console.log(props.directions.routes[0].legs[0].steps[10].instructions);
 
-const DirectionInstructions = (props)=>{
-    if(props.directions.routes){
-        console.log(props.directions.routes[0].legs[0].steps[10].instructions)
-        
-        
-        return(
-     
-     
-            this.props.directions.routes.map((instructions)=>{
-
-                return(
-<h3>
-                         <h3>{instructions}</h3>
-                    </h3>
-                )
-                    
-        })
-        )
-    }
-}
+    return this.props.directions.routes.map((instructions) => {
+      return (
+        <div>
+          <h3>{instructions}</h3>
+        </div>
+      );
+    });
+  }
+};
 
 export default DirectionInstructions;
--- gpsmark/src/components/Map.js
+++ gpsmark/src/components/Map.js
@@ -4,141 +4,162 @@ import {
   withGoogleMap,
   withScriptjs,
   GoogleMap,
-  DirectionsRenderer
+  DirectionsRenderer,
 } from "react-google-maps";
 import Geocode from "react-geocode";
-import DirectionInstructions from './Instructions.js'
 
+Geocode.setApiKey("AIzaSyCTIuglr0yoyg7N3YinUj0xEeKmQCA8VT0");
+Geocode.enableDebug();
 
-
-
- Geocode.setApiKey("AIzaSyCTIuglr0yoyg7N3YinUj0xEeKmQCA8VT0");
- Geocode.enableDebug();
-
-
-class Map extends Component {
+class Map extends React.Component {
   state = {
     directions: [],
-    directionsLocal: [],
-    latOrigin:0,
-    lngOrigin:0,
-    latDestin:0,
-    lngDestin:0,
-    
+    destination: "",
+    latOrigin: 0,
+    lngOrigin: 0,
+    latDestin: 0,
+    lngDestin: 0,
+    destinationAddress: "",
   };
 
-    geoDestiny = ()=>{
-     
-      Geocode.fromAddress("california").then(
-        response => {
-          let { lat, lng } = response.results[0].geometry.location;
-          this.setState({
-            latDestin:lat,
-            lngDestin:lng,
-          })
-          console.log(this.state.latDestin, this.state.lngDestin);
-          this.getpositon();
-        },
-        error => {
-          console.error(error);
-        }
-      );
-    }
-   
-     displayLocationInfo=(position)=>{
-     
-        let lng2 = position.coords.longitude;
-        let lat2 = position.coords.latitude;
-        console.log(`longitude: ${ lat2 } | latitude: ${ lng2 }`);
-       this.setState({
-        latOrigin: lat2,
-        lngOrigin:lng2
-       })
-       console.log('disply'+this.state.lat)
-       
-       this.loadMap()
-       
-     }
-    
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // this.setState({
+    //   destinationAddress: this.state.destinationAddress,
+    // });
+    console.log(this.state.destinationAddress);
+    this.geoDestiny();
+  };
 
-  getpositon=()=>{
- 
+  geoDestiny = () => {
+    Geocode.fromAddress(this.state.destinationAddress).then(
+      (response) => {
+        let { lat, lng } = response.results[0].geometry.location;
+        this.setState({
+          latDestin: lat,
+          lngDestin: lng,
+        });
+        console.log(this.state.latDestin, this.state.lngDestin);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  };
+
+  displayLocationInfo = (position) => {
+    let lng2 = position.coords.longitude;
+    let lat2 = position.coords.latitude;
+    console.log(`longitude: ${lat2} | latitude: ${lng2}`);
+    this.setState({
+      latOrigin: lat2,
+      lngOrigin: lng2,
+    });
+    console.log("disply" + this.state.lat);
+
+    this.loadMap();
+  };
+
+  getpositon = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
-      
-    } 
+    }
+  };
 
-  }
+  loadMap = () => {
+    console.log(
+      `origin lat: ${this.state.latOrigin}, lng: ${this.state.lngOrigin} `
+    );
+    console.log(
+      `Destino lat:  ${this.state.latDestin}, lng: ${this.state.lngDestin} `
+    );
 
-  loadMap=()=>{
-    console.log(`origin lat: ${this.state.latOrigin}, lng: ${this.state.lngOrigin} `)
-    console.log(`Destino lat:  ${this.state.latDestin}, lng: ${this.state.lngDestin} `)
-    
     let origin = { lat: this.state.latOrigin, lng: this.state.lngOrigin };
-    let destination = { lat:  this.state.latDestin, lng: this.state.lngDestin };
-    const directionsService =  new google.maps.DirectionsService();
+    let destination = { lat: this.state.latDestin, lng: this.state.lngDestin };
+    const directionsService = new google.maps.DirectionsService();
     directionsService.route(
       {
         origin: origin,
         destination: destination,
-        travelMode: google.maps.TravelMode.DRIVING
+        travelMode: google.maps.TravelMode.DRIVING,
       },
       (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
           this.setState({
-            directions: result
-
+            directions: result,
           });
         } else {
           console.error(`error fetching directions ${result}`);
         }
       }
     );
-  }
+  };
 
-  componentDidMount = async ()=> {
-    this.geoDestiny()
-    
-
-    console.log('component'+this.state.lat)
-    
-
-  }
+  componentDidMount = () => {
+    console.log("component" + this.state.lat);
+  };
 
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
+    const GoogleMapExample = withGoogleMap((props) => (
       <GoogleMap
         defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
         defaultZoom={13}
-      > 
-        <DirectionsRenderer directions={this.state.directions}/>
-       
+      >
+        <DirectionsRenderer directions={this.state.directions} />
       </GoogleMap>
     ));
-    // console.log(this.state.directions)
 
-    // if(this.state.directions.routes ){
-       
-
-    //     console.log('routes ',this.state.directions.routes[0].legs[0].steps[0].instructions)
-        
-    // }
-    
-    // {console.log(this.state.directions.routes)}
     return (
       <div>
-             
+        {this.state.destinationAddress != "" ? (
+          <GoogleMapExample
+            containerElement={
+              <div style={{ height: `500px`, width: "500px" }} />
+            }
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        ) : (
+          ""
+        )}
 
-        
-
-        <GoogleMapExample          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-       <div>
-       {this.state.directions.routes ? this.state.directions.routes[0].legs[0].steps.map((elemen)=> (<div> <p dangerouslySetInnerHTML={{ __html: elemen.instructions }}/> </div>)) :''}
-       </div>
-         
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <lable htmlFor="description" className="col-sm-1 col-form-label">
+              Destination Address:
+            </lable>
+            <div class="col-sm-11">
+              <input
+                type="text"
+                required
+                value={this.state.destinationAddress}
+                className="form-control"
+                id="destinationAddress"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div class="col-sm-10">
+              <button type="submit" class="btn btn-primary">
+                Create{" "}
+              </button>
+            </div>
+          </div>
+        </form>
+        <div>
+          {this.state.directions.routes
+            ? this.state.directions.routes[0].legs[0].steps.map((elemen) => (
+                <div>
+                  {" "}
+                  <p
+                    dangerouslySetInnerHTML={{ __html: elemen.instructions }}
+                  />{" "}
+                </div>
+              ))
+            : ""}
+        </div>
       </div>
     );
   }
