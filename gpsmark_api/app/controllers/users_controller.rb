49e131b384729e9f2db448253ec353fe5cab44ca
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Pro.local>
2020-07-29 23:53:23 -0400

    adding routes in ruby on rails

--- gpsmark_api/app/controllers/users_controller.rb
+++ gpsmark_api/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users.to_json(include: :places)
   end
 
   # GET /users/1
--- gpsmark_api/db/seeds.rb
+++ gpsmark_api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([
+  { user: 'Alex', password: '12346' },
+  { user: 'Brittney', password: '23456' }
+  
+])
+
+Place.create([
+  { nick_name:'My house', addrees: 'miami', user_id: 1 },
+  { nick_name:'My work', addrees: '316 cameron bridge way', user_id: 1 },
+  { nick_name:'My house', addrees: 'atlanta', user_id: 2 }
+ 
+])
